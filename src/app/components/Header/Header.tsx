3f969ca4c726a7a7eb3e86cd1b050fa12c109173
Dmitrii_Porubaev <zdima1543@gmail.com>
2025-02-19 12:28:03 +0600

    d

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+import { FC } from 'react';
+
+export const Header: FC = () => {
+    return (
+        <header>
+            <Link href="#" />
+        </header>
+    );
+};
+
---
+++ src/app/pages/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FC } from 'react';
+
+export const Hero: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
